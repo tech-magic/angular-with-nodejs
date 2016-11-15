eb3cd1afa2fcc70bfc9c36a55d017fe462df4e0d
wimal perera <wimal.perera@ebuilder.com>
2016-11-15 11:36:56 +0530

    i18n enhancements

--- public/javascripts/translate.js
+++ public/javascripts/translate.js
@@ -1,22 +1,22 @@
 angular.module('tMTranslate', ['pascalprecht.translate'])
 	.config(['$translateProvider', function($translateProvider) {
 		
-		/*$translateProvider.translations('en', {
-			HELP_TEXT_NO_OF_ACTIVE_USERS: 'Welcome to the angular-translate API reference. If you want to know how angular-translate works, checkout the developer guide!',
-			TITLE_NO_OF_ACTIVE_USERS: 'No of active users'
-		});
+		var fileNameConvention = {
+    		prefix: '/public/i18n/locale-',
+    		suffix: '.json'
+  		};
 
-		$translateProvider.translations('de', {
-			HELP_TEXT_NO_OF_ACTIVE_USERS: 'Willkommen in der Winkel übersetzen API-Referenz . Wenn Sie wissen wollen, wie Werke Winkel übersetzen , Check-out die Entwicklerhandbuch  !',
-			TITLE_NO_OF_ACTIVE_USERS: 'Anzahl der aktiven Benutzer'
-		});*/
+		var langMap = {
+      	  'en-AU': 'en',
+      	  'en-CA': 'en',
+      	  'en-NZ': 'en',
+      	  'en-PH': 'en',
+      	  'en-UK': 'en',
+      	  'en-US': 'en'
+        };
 
-		$translateProvider.useStaticFilesLoader({
-			files: [{
-				prefix: '/public/i18n/locale-',
-				suffix: '.json'
-			}]
-		});
-
-		$translateProvider.preferredLanguage('en');
+		$translateProvider.useStaticFilesLoader(fileNameConvention)
+    		.registerAvailableLanguageKeys(['en'], langMap)
+    		.determinePreferredLanguage()
+    		.fallbackLanguage(['en']);
 	}]);
